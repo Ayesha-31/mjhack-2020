ea8ff9ae30279bdad4ff1be5a31c88a712e8c9fd
Arish-Shah <arish0246@gmail.com>
2020-01-02 22:30:33 +0530

    fixes #1

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const drawer = document.querySelector('#drawer');
+const openDrawerButton = document.querySelector('#open-drawer-button');
+const closeDrawerButton = document.querySelector('#close-button');
+
+openDrawerButton.addEventListener('click', function() {
+  console.log('drawer opened');
+});
