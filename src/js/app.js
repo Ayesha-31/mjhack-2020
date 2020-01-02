ad9d3a9a8c2c9c5e3a2ad55fe756f4f34575bb32
Arish-Shah <arish0246@gmail.com>
2020-01-02 23:21:30 +0530

    fixes #2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
-const drawer = document.querySelector('#drawer');
-const openDrawerButton = document.querySelector('#open-drawer-button');
-const closeDrawerButton = document.querySelector('#close-button');
+const drawer = document.querySelector('.navigation-drawer');
+const openDrawerButton = document.querySelector('#hamburger');
+const closeDrawerButton = document.querySelector('#cross');
+const navbarEnd = document.querySelector('.navbar-end');
 
-openDrawerButton.addEventListener('click', function() {
-  console.log('drawer opened');
-});
+openDrawerButton.onclick = function() {
+  navbarEnd.style.display = 'none';
+  drawer.style.display = 'initial';
+  closeDrawerButton.style.display = 'initial';
+};
+
+closeDrawerButton.onclick = function() {
+  navbarEnd.style.display = 'initial';
+  drawer.style.display = 'none';
+  closeDrawerButton.style.display = 'none';
+};
