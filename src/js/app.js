29795b08ddcc95293edb1d6ebd9dedec263f173a
Arish-Shah <arish0246@gmail.com>
2020-01-03 12:24:34 +0530

    added timer

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,17 @@ const openDrawerButton = document.querySelector('#hamburger');
 const closeDrawerButton = document.querySelector('#cross');
 const navbarEnd = document.querySelector('.navbar-end');
 
-openDrawerButton.onclick = function() {
+const openDrawer = () => {
   navbarEnd.style.display = 'none';
   drawer.style.display = 'initial';
   closeDrawerButton.style.display = 'initial';
 };
 
-closeDrawerButton.onclick = function() {
+const closeDrawer = () => {
   navbarEnd.style.display = 'initial';
   drawer.style.display = 'none';
   closeDrawerButton.style.display = 'none';
 };
+
+openDrawerButton.addEventListener('click', openDrawer);
+closeDrawerButton.addEventListener('click', closeDrawer);
