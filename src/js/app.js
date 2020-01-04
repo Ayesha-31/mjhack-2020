4ac3a14a97511d313fd2a40b3ff235e8924c9257
Arish-Shah <arish0246@gmail.com>
2020-01-05 00:09:36 +0530

    --

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,23 @@ const closeDrawerButton = document.querySelector('#cross');
 const navbarEnd = document.querySelector('.navbar-end');
 const registerButtons = document.querySelectorAll('.register-button');
 
+// Drawer closing
+const navDrawer = document.querySelector('.navigation-drawer');
+const navDrawerLinks = navDrawer.querySelectorAll('a');
+const navDrawerButton = navDrawer.querySelector('button');
+
 const openDrawer = () => {
   navbarEnd.style.display = 'none';
   drawer.style.display = 'initial';
   closeDrawerButton.style.display = 'initial';
+  document.body.style.position = 'fixed';
 };
 
 const closeDrawer = () => {
   navbarEnd.style.display = 'flex';
   drawer.style.display = 'none';
   closeDrawerButton.style.display = 'none';
+  document.body.style.position = 'static';
 };
 
 const registerHash = () => {
@@ -26,6 +33,11 @@ closeDrawerButton.addEventListener('click', closeDrawer);
 registerButtons.forEach(registerButton => {
   registerButton.addEventListener('click', registerHash);
 });
+
+navDrawerLinks.forEach(navDrawerLink =>
+  navDrawerLink.addEventListener('click', closeDrawer)
+);
+navDrawerButton.addEventListener('click', closeDrawer);
 
 if (module.hot) {
   module.hot.accept();
